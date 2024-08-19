8a3b295307ac84cbb6d24e8f4cd5fd2357aa00c3
carsumin <81725519+carsumin@users.noreply.github.com>
2024-08-19 22:47:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char x = s.charAt(0);
+        
+        System.out.println((int)x);
+    }
+}
