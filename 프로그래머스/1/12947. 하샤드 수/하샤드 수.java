0b08c58c68a2cb549cf3cc769fe0a2bcb3bd97e2
carsumin <81725519+carsumin@users.noreply.github.com>
2024-08-22 22:43:35 +0900

    [level 1] Title: 하샤드 수, Time: 0.20 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        String[] str = Integer.toString(x).split("");
+        int sum = 0;
+        
+        for(int i=0; i<str.length; i++){
+            sum += Integer.parseInt(str[i]);
+        }
+        
+        if(x%sum==0){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
