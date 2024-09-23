f927a33d186f1f4999dbf15489159eb5893cc6f9
carsumin <81725519+carsumin@users.noreply.github.com>
2024-09-23 22:20:49 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        // replace
+        answer = my_string.replace(letter, "");
+        
+        return answer;
+    }
+}
