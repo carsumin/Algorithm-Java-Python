fdb9ea647d0a66c02df899f4c500f87ea25a918b
carsumin <81725519+carsumin@users.noreply.github.com>
2024-10-17 15:55:24 +0900

    [Bronze II] Title: 진법 변환, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        int b = sc.nextInt();
+        
+        System.out.println(Integer.parseInt(n, b));
+    }
+}
